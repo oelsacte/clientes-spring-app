d9da570414819b8fe15e2d7097e43fb1090a01f6
Octavio Elsacte Palma <oelsacte@gmail.com>
2019-11-11 01:53:32 -0600

    Primer commit

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.bolsadeideas.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.repository.CrudRepository;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
+
+}
---
+++ target/classes/import.sql
@@ -0,0 +1,25 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2018-01-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2018-01-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2018-02-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2018-02-10');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2018-02-18');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2018-02-28');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dr. James', 'Gosling', 'james.gosling@gmail.com', '2018-03-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andrés2', 'Guzmán2', 'profesor2@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Mr. John2', 'Doe2', 'john.doe2@gmail.com', '2018-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linus2', 'Torvalds2', 'linus.torvalds2@gmail.com', '2018-01-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rasmus2', 'Lerdorf2', 'rasmus.lerdorf2@gmail.com', '2018-01-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Erich2', 'Gamma2', 'erich.gamma2@gmail.com', '2018-02-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard2', 'Helm2', 'richard.helm2@gmail.com', '2018-02-10');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ralph2', 'Johnson2', 'ralph.johnson2@gmail.com', '2018-02-18');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John2', 'Vlissides2', 'john.vlissides2@gmail.com', '2018-02-28');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dr. James2', 'Gosling2', 'james.gosling2@gmail.com', '2018-03-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma2', 'Lee2', 'magma.lee2@gmail.com', '2018-03-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado2', 'Roe2', 'tornado.roe2@gmail.com', '2018-03-05');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade2', 'Doe2', 'jane.doe2@gmail.com', '2018-03-06');
